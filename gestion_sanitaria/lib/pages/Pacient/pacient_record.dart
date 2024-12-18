d862ef2269700eb2bc815e6ef5a4d329a03daebb
Celot1979 <zata_k@hotmail.com>
2024-12-18 12:09:14 +0100

    Ref_Record_Pacient

--- gestion_sanitaria/lib/pages/Pacient/pacient_record.dart
+++ gestion_sanitaria/lib/pages/Pacient/pacient_record.dart
@@ -15,17 +15,20 @@ class PacientRecord extends StatelessWidget {
   
   get firebaseService => null;
 
+  // Método para inicializar los controladores
+  List<TextEditingController> _initializeControllers() {
+    return [
+      TextEditingController(text: " "),
+      TextEditingController(text: " "),
+      TextEditingController(text: " "),
+      TextEditingController(text: " "),
+      TextEditingController(text: " "),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-     TextEditingController roomControler = TextEditingController(text: " ");
-     TextEditingController full_nameControler = TextEditingController(text: " ");
-     TextEditingController pathologyControler = TextEditingController(text: " ");
-     TextEditingController timeControler = TextEditingController(text: " ");
-     TextEditingController name_medicationControler = TextEditingController(text: " ");
-
-    // Se elimina la instancia de FirebaseServicePacient debido a que no está definida
-
-   
+    final controllers = _initializeControllers();
 
     return Scaffold(
       appBar: CustomAppBar(
@@ -41,161 +44,91 @@ class PacientRecord extends StatelessWidget {
         child: Form(
           child: Column(
             children: [
-              CustomTextField(
-              controller: roomControler,
-              hintText: "add room number",
-              prefixIcon: Icons.room_outlined,
-              keyboardType: TextInputType.name,
-              onTap: () {}, // Agregado el argumento requerido
-            ),
+              ..._buildTextFields(controllers),
               const SizedBox(height: 20),
-              CustomTextField(
-              controller: full_nameControler,
-              hintText: "add full name",
-              prefixIcon: Icons.person_outline,
-              keyboardType: TextInputType.name,
-              onTap: () {}, // Agregado el argumento requerido
-            ),
-              const SizedBox(height: 20),
-              CustomTextField(
-              controller: pathologyControler,
-              hintText: "add Pathology",
-              prefixIcon: Icons.health_and_safety,
-              keyboardType: TextInputType.name,
-              onTap: () {}, // Agregado el argumento requerido
-            ),
-            const SizedBox(height: 20),
-              CustomTextField(
-              controller: name_medicationControler,
-              hintText: "add name medication",
-              prefixIcon: Icons.health_and_safety,
-              keyboardType: TextInputType.name,
-              onTap: () {}, // Agregado el argumento requerido
-            ),
-              const SizedBox(height: 20),
-              CustomTextField(
-              controller: timeControler,
-              hintText: "add Medication Time (YYYY-MM-DD HH:MM)",
-              prefixIcon: Icons.timelapse_sharp,
-              keyboardType: TextInputType.datetime,
-              onTap: () {}, // Agregado el argumento requerido
-            ),
-            
-              const SizedBox(height: 20),
-              // Agregando los botones en un contenedor
-              /*Container(
-                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      width: 150, // Estableciendo el ancho del botón
-                      child: CustomButton(
-                        text: "Add Pacient",
-                        onPressed: () async {
-                          await addpacientP(
-                            roomControler.text,
-                            full_nameControler.text,
-                            pathologyControler.text,
-                            timeControler.text,
-                            name_medicationControler.text,
-                          );
-                           roomControler.clear();
-                           full_nameControler.clear();
-                           pathologyControler.clear();
-                           timeControler.clear();
-                           name_medicationControler.clear();
-
-                          
-                        },
-                        
-                      ),
-                    ),
-                    /*SizedBox(
-                      width: 150, // Estableciendo el ancho del botón
-                      child: CustomButton(
-                        text: "Update information pacient",
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const PacientUpdate()), 
-                            /* Lo hemos dejado aquí:
-                            1º Crear un nuevo archivo que se llame update_paciente.dart
-                            2º Cambiamos el nombre de la clase que llama dentro del Navigaitor.plus.
-                            3º En el nuevo archivo - update_paciente.dart - tenemos que copiar y pegar el archivo
-                            que tenemos de modificar registros. 
-                            4º Crearemos las instrucciones en el archivo de firebase_servicie_pacient oportunas para
-                            que ejecuten las ordenes pertinentes. */
-                          );
-                        },
-                      ),
-                    ),*/
-                    
-                    
-                    
-                  ],
-                  
-
-                ),
-                
-                
-              ),*/
-             
+              // Aquí puedes agregar los botones si es necesario
             ],
-            
-            
           ),
-          
         ),
-        
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.blue,
-        selectedItemColor: Colors.lightBlueAccent,
-        unselectedItemColor: Colors.white,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_add, color: Colors.white),
-            label: 'ADD PACIENT',
+      bottomNavigationBar: _buildBottomNavigationBar(context, controllers),
+    );
+  }
+
+  List<Widget> _buildTextFields(List<TextEditingController> controllers) {
+    final hints = [
+      "add room number",
+      "add full name",
+      "add Pathology",
+      "add name medication",
+      "add Medication Time (YYYY-MM-DD HH:MM)"
+    ];
+    final icons = [
+      Icons.room_outlined,
+      Icons.person_outline,
+      Icons.health_and_safety,
+      Icons.health_and_safety,
+      Icons.timelapse_sharp
+    ];
+
+    return List.generate(hints.length, (index) {
+      return Column(
+        children: [
+          CustomTextField(
+            controller: controllers[index],
+            hintText: hints[index],
+            prefixIcon: icons[index],
+            keyboardType: index == 4 ? TextInputType.datetime : TextInputType.name,
+            onTap: () {},
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.update, color: Colors.white),
-            label: 'UPDATE INFORMATION',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map, color: Colors.white),
-            label: 'Map',
-          ),
+          const SizedBox(height: 20),
         ],
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              // Lógica para agregar paciente
-              addpacientP(
-                roomControler.text,
-                full_nameControler.text,
-                pathologyControler.text,
-                timeControler.text,
-                name_medicationControler.text,
-              );
-              roomControler.clear();
-              full_nameControler.clear();
-              pathologyControler.clear();
-              timeControler.clear();
-              name_medicationControler.clear();
-              break;
-            case 1:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const PacientUpdate()),
-              );
-              break;
-            case 2:
-              Navigator.pushNamed(context, '/login');
-              break;
-          }
-        },
-      ),
+      );
+    });
+  }
+
+  BottomNavigationBar _buildBottomNavigationBar(BuildContext context, List<TextEditingController> controllers) {
+    return BottomNavigationBar(
+      backgroundColor: Colors.blue,
+      selectedItemColor: Colors.lightBlueAccent,
+      unselectedItemColor: Colors.white,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_add, color: Colors.white),
+          label: 'ADD PACIENT',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.update, color: Colors.white),
+          label: 'UPDATE INFORMATION',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map, color: Colors.white),
+          label: 'Map',
+        ),
+      ],
+      onTap: (index) {
+        switch (index) {
+          case 0:
+            addpacientP(
+              controllers[0].text,
+              controllers[1].text,
+              controllers[2].text,
+              controllers[3].text,
+              controllers[4].text,
+            );
+            controllers.forEach((controller) => controller.clear());
+            break;
+          case 1:
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const PacientUpdate()),
+            );
+            break;
+          case 2:
+            Navigator.pushNamed(context, '/login');
+            break;
+        }
+      },
     );
   }
 }
